45dd4197f6a8e871194482462e9e2c34f6d894f7
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-11-30 20:27:58 -0300

    primeiro quase feito

--- desafio1.sql
+++ desafio1.sql
@@ -1,26 +1,84 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
-  CREATE DATABASE IF NOT EXISTS SpotifyClone ;
-
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
+  CREATE DATABASE IF NOT EXISTS SpotifyClone;
+  
+CREATE TABLE SpotifyClone.User(
+      id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+      username VARCHAR(100) NOT NULL,
+      age INT NOT NULL
   ) engine = InnoDB;
 
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
+CREATE TABLE SpotifyClone.Plan(
+      plan_name VARCHAR(100) NOT NULL,
+      value_plan DECIMAL(19,2),
+   User_id_plan INT,
+    FOREIGN KEY (User_id_plan)
+    REFERENCES SpotifyClone.User (id_user)
+ )engine = InnoDB;
 
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+ CREATE TABLE SpotifyClone.Artist (
+  id_artist INT NOT NULL PRIMARY KEY,
+  artist_name VARCHAR(100) NOT NULL,
+ )ENGINE = InnoDB;
 
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
+ CREATE TABLE SpotifyClone.Album (
+  id_album INT NOT NULL PRIMARY KEY,
+  name_album VARCHAR(100) NOT NULL,
+  artist_id_artist INT,
+    FOREIGN KEY (artist_id_artist)
+    REFERENCES SpotifyClone.Artist (id_artist)
+)ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS SpotifyClone.Musics (
+  id_music INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  music_name VARCHAR(255) NOT NULL,
+  album_id_album INT NOT NULL,
+  duration INT NOT NULL,
+    FOREIGN KEY (album_id_album)
+    REFERENCES SpotifyClone.Album (`id_album`)
+)ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS SpotifyClone.History (
+  datetime DATETIME NOT NULL,
+  users_id_users INT NOT NULL,
+  musics_id_music INT NOT NULL,
+  musics_id_history INT NOT NULL,
+    FOREIGN KEY (users_id_users)
+    REFERENCES SpotifyClone.User (id_user),
+    FOREIGN KEY (musics_id_music)
+    REFERENCES SpotifyClone.Musics (id_music)
+)ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS SpotifyClone.Follow (
+  artista_id_artist INT,
+  users_id_users INT,
+    FOREIGN KEY (artista_id_artist)
+    REFERENCES SpotifyClone.Artista (id_artist),
+    FOREIGN KEY (users_id_users)
+    REFERENCES SpotifyClone.User (id_user)
+)ENGINE = InnoDB;
+
+   INSERT INTO SpotifyClone.Plan (plan_name, value_plan)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+    ('gratuito', '0.00'),
+    ('gratuito', '0.00'),
+    ('familiar', '7.99'),
+    ('familiar', '7.99'),
+    ('familiar', '7.99'),
+    ('universitário', '5.99'),
+    ('universitário', '5.99'),
+    ('pessoal', '6.99'),
+    ('pessoal', '6.99'),
+    ('pessoal', '6.99');
+
+  INSERT INTO SpotifyClone.User (username, age) VALUES
+    ('Barbara Liskov', '82'),
+    ('Robert Cecil Martin', '58'),
+    ('Ada Lovelace', '37'),
+    ('Martin Fowler', '46'),
+    ('Sandi Metz', '58'),
+    ('Paulo Freire', '19'),
+    ('Bell Hooks', '26'),
+    ('Christopher Alexander', '85'),
+    ('Judith Butler', '45'),
+    ('Jorge Amado', '58');
